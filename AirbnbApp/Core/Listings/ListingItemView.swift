61337873df9bb07b4719d304ecf1db61fc47416f
Carlos Alcala <carlos.alcala@me.com>
2024-11-22 17:35:01 -0400

    Implemented ListingView with Listing House Images

--- AirbnbApp/Core/Listings/ListingItemView.swift
+++ AirbnbApp/Core/Listings/ListingItemView.swift
@@ -7,11 +7,26 @@
 
 import SwiftUI
 struct ListingItemView: View {
+    var images = [
+        "listing-1",
+        "listing-2",
+        "listing-3",
+        "listing-4"
+    ]
+
     var body: some View {
         VStack(spacing: 8) { // images
-            Rectangle()
-                .frame(height: 320)
-                .clipShape (RoundedRectangle(cornerRadius: 10))
+            TabView() {
+                ForEach(images, id: \.self) { image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFill()
+                }
+            }
+            .frame(height: 320)
+            .clipShape (RoundedRectangle(cornerRadius: 10))
+            .tabViewStyle(.page)
+
             // listing details
             HStack(alignment: .top) {
                 // details
@@ -36,7 +51,6 @@ struct ListingItemView: View {
             }
             .font(.footnote)
         }
-        .padding()
     }
 }
 
